36129f7d2a42cfb8557617cbae9d9496b8b7cd76
marciaDelCastillo <marcia990_14@hotmail.com>
2022-02-25 01:03:32 -0300

    integracion bootstrap,header,footer e index

--- Desktop/PROGRAMACION/PROYECTO FREELANCER/loop/routes/index.js
+++ Desktop/PROGRAMACION/PROYECTO FREELANCER/loop/routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'DELFOS S.A.' });
+});
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio', { title: 'PORTAFOLIO' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'CONTACTO' });
 });
 
 module.exports = router;
